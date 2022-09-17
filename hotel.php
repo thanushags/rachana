407f75c00db4b5d637d1ee631f27999916530688
thanushags <98949476+thanushags@users.noreply.github.com>
2022-09-17 13:04:53 +0530

    Add files via upload

---
+++ hotel.php
@@ -0,0 +1,168 @@
+
+
+<?php
+// Output messages
+$responses = [];
+// Check if the form was submitted
+if (isset($_POST['arrival'], $_POST['departure'], $_POST['first_name'], $_POST['last_name'], $_POST['email'], $_POST['phone'], $_POST['adults'], $_POST['children'], $_POST['room_pref'])) {
+	// Process form data
+}
+// Assign POST variables
+$arrival = htmlspecialchars($_POST['arrival'], ENT_QUOTES);
+$departure = htmlspecialchars($_POST['departure'], ENT_QUOTES);
+$first_name = htmlspecialchars($_POST['first_name'], ENT_QUOTES);
+$last_name = htmlspecialchars($_POST['last_name'], ENT_QUOTES);
+$email = htmlspecialchars($_POST['email'], ENT_QUOTES);
+$phone = htmlspecialchars($_POST['phone'], ENT_QUOTES);
+$adults = htmlspecialchars($_POST['adults'], ENT_QUOTES);
+$children = htmlspecialchars($_POST['children'], ENT_QUOTES);
+$room_pref = htmlspecialchars($_POST['room_pref'], ENT_QUOTES);
+// Validate email adress
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	$responses[] = 'Email is not valid!';
+}
+// First name must contain only alphabet characters.
+if (!preg_match('/^[a-zA-Z]+$/', $first_name)) {
+	$responses[] = 'First name must contain only characters!';
+}
+// Last name must contain only alphabet characters.
+if (!preg_match('/^[a-zA-Z]+$/', $last_name)) {
+	$responses[] = 'Last name must contain only characters!';
+}
+
+// If there are no errors
+if (!$responses) {
+	// Where to send the mail? It should be your email address
+	$to      = 'reservations@yourwebsite.com';
+	// Mail from
+	$from    = 'noreply@yourwebsite.com';
+	// Mail subject
+	$subject = 'A guest has booked a reservation';
+	// Mail headers
+	$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'Return-Path: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+	// Capture the email template file as a string
+	ob_start();
+	//include 'email-template.php';
+	//$email_template = ob_get_clean();
+	// Try to send the mail
+	//if (mail($to, $subject, $email_template, $headers)) {
+		// Success
+	//	$responses[] = 'Thank you for your reservation!';		
+	//} else {
+		// Fail; problem with the mail server...
+	//	$responses[] = 'Message could not be sent! Please check your mail server settings!';
+		}	//}
+?>
+
+
+<?php if ($responses): ?>
+<p class="responses"><?php echo implode('<br>', $responses); ?></p>
+<?php endif; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,minimum-scale=1">
+		<title>Homestay Booking Form</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<form class="hotel-reservation-form" method="post" action="">
+			<h1><i class="far fa-calendar-alt"></i>Homestay Reservation Form</h1>
+			<div class="fields">
+				<!-- Input Elements -->
+				<div class="wrapper">
+	<div>
+		<label for="arrival">Arrival</label>
+		<div class="field">
+			<input id="arrival" type="date" name="arrival" required>
+		</div>
+	</div>
+	<div class="gap"></div>
+	<div>
+		<label for="departure">Departure</label>
+		<div class="field">
+			<input id="departure" type="date" name="departure" required>
+		</div>
+	</div>
+</div>
+
+<div class="wrapper">
+	<div>
+		<label for="first_name">First Name</label>
+		<div class="field">
+			<i class="fas fa-user"></i>
+			<input id="first_name" type="text" name="first_name" placeholder="First Name" required>
+		</div>
+	</div>
+	<div class="gap"></div>
+	<div>
+		<label for="last_name">Last Name</label>
+		<div class="field">
+			<i class="fas fa-user"></i>
+			<input id="last_name" type="text" name="last_name" placeholder="Last Name" required>
+		</div>
+	</div>
+</div>
+
+
+<label for="email">Email</label>
+<div class="field">
+	<i class="fas fa-envelope"></i>
+	<input id="email" type="email" name="email" placeholder="Your Email" required>
+</div>
+
+<label for="phone">Phone</label>
+<div class="field">
+	<i class="fas fa-phone"></i>
+	<input id="phone" type="tel" name="phone" placeholder="Your Phone Number" required>
+</div>
+<div class="wrapper">
+	<div>
+		<label for="adults">Adults</label>
+		<div class="field">
+			<select id="adults" name="adults" required>
+				<option disabled selected value="">--</option>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+			</select>
+		</div>
+	</div>
+	<div class="gap"></div>
+	<div>
+		<label for="children">Children</label>
+		<div class="field">
+			<select id="children" name="children" required>
+				<option disabled selected value="">--</option>
+				<option value="0">0</option>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+			</select>
+		</div>
+	</div>
+</div>
+<option value="5">5</option>
+
+
+
+<label for="room_pref">Room Preference</label>
+<div class="field">
+	<select id="room_pref" name="room_pref" required>
+		<option disabled selected value="">--</option>
+		<option value="Standard">Standard</option>
+		<option value="Deluxe">Deluxe</option>
+		<option value="Suite">Suite</option>
+	</select>
+</div>
+<input type="submit" value="Reserve">
+
+			</div>
+		</form>
+	</body>
+</html>
